b6719cf5cc8765c773ca951b4d75fec957f9c0d3
Leonid Gorkin <regnull@gmail.com>
2025-03-18 10:31:34 -0400

    fix build

--- worker.py
+++ worker.py
@@ -13,7 +13,7 @@ from pathlib import Path
 from t2v_utils import text_to_video
 from i2v_utils import image_to_video
 from nats.aio.client import Client as NATS
-from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers, TimeoutError
+from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 
 
 def maybe_load_env():
